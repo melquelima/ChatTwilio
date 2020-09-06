bc4c075ab44f7b5bff03a9b7db9398813a79f25a
melque <melque_ex@yahoo.com.br>
2020-09-06 14:55:49 -0300

    update

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -2,19 +2,65 @@ from app import app
 from flask import request
 from app.models import *
 from twilio.twiml.messaging_response import MessagingResponse
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+
 
 @app.route("/")
 def home():
     return "OK"
 
 
+def sendEmail():
+    msgRoot = MIMEMultipart('related')
+    msgRoot['From'] = "memelyma@gmail.com"
+    msgRoot['To'] = Email.query.first().email
+    msgRoot['Subject'] = "Respostas Colhidas pelo chatBot"
+    msg = MIMEMultipart('alternative')
+
+    lst = Status.query.all()
+    style= "<style>table {{font-family: arial, sans-serif;border-collapse: collapse;width: 100%;}}td, th {{border: 1px solid #dddddd;text-align: left;padding: 8px;}}tr:nth-child(even) {{background-color: #dddddd;}}</style>"
+    header = style + "<table><thead><tr>{}</tr></thead><tbody>{}</tbody></table>"
+    mask1 = "<th>{}</th>"
+    mask2 = "<td>{}</td>\n<td>{}</td>"
+    mask3 = "<tr>{}</tr>"
+    head = "\n".join([mask1.format(x) for x in ['pergunta','resposta']])
+    body = "\n".join([mask3.format(y) for y in [mask2.format(x.msg.text.replace("\n","<br>"),x.resposta) for x in lst]])
+
+    bd = header.format(head,body).replace("\n","")
+    message = bd.replace("Perfeito! Senguem minhas sugestões:\\sLink:https://images.unsplash.com/photo-1518717758536-85ae29035b6d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80\\sLink:https://images.unsplash.com/photo-1545093149-618ce3bcf49d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80\\s","")
+    message = "Segue o resumo das respostas colhidas pelo chatbot<br><br><br><br>" + message
+
+    msg.attach(MIMEText(message, 'html'))
+    msgRoot.attach(msg)
+
+    server = smtplib.SMTP_SSL('smtp.gmail.com')
+    server.login("memelyma@gmail.com", "qwerty333")
+    
+    server.sendmail(msgRoot['From'],msgRoot['To'],msgRoot.as_string())
+    # server.sendmail("memelyma@gmail.com", "melque_ex@yahoo.com.br", bd.encode('utf-8'))
+    server.quit()
+
+    return 
+
+
 @app.route("/sms", methods=['GET', 'POST'])
 def sms_reply():
     resp = MessagingResponse()
     received_message = request.form.get("Body")
+    if "@" in received_message :
+        Email.query.delete()
+        Email(received_message).save()
+        resp.message(f"email '{received_message}' cadastrado com sucesso!")
+        return str(resp)
+
+
+
 
     last = Status.query.order_by(Status.id.desc()).first()
-    if last and last.msg.opcoes == "end":
+    if (last and last.msg.opcoes == "end") or received_message == "*":
         Status.query.delete()
         db.session.commit()
         last = None
@@ -29,7 +75,8 @@ def sms_reply():
         if last.resposta is None:
             aceitas = last.msg.opcoes.split(",")
             if received_message in aceitas or "*" in aceitas:
-                last.resposta = received_message
+                r = last.msg.getResponse(received_message)
+                last.resposta = r
                 last.save()
                 nextQuestion(last,resp)
             else:
@@ -37,8 +84,6 @@ def sms_reply():
                 last.msg.send(resp)
         else:
             nextQuestion(last,resp)
-
-
     return str(resp)
 
 
@@ -46,3 +91,7 @@ def nextQuestion(last,resp):
     p = Perguntas.query.filter_by(ordem=last.msg.ordem + 1 ).first()
     p.send(resp)
     Status(p.id).save()
+    if p.opcoes == 'end':
+        sendEmail()
+        
+
--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,23 @@
 from app import db
 from sqlalchemy import Float,Column,Integer,String,ForeignKey,DateTime,Time,Boolean
+import re
+
+GOOD_BOY_URL = "https://images.unsplash.com/photo-1518717758536-85ae29035b6d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+
+class Email(db.Model):
+    __tablename__ = "email"
+
+    id          = Column(Integer(),primary_key=True)
+    email       = Column(Integer(),nullable=False,unique=True)
+
+    def __init__(self,email):
+        self.email = email
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()    
+
+
 
 class Perguntas(db.Model):
     __tablename__ = "perguntas"
@@ -18,7 +36,19 @@ class Perguntas(db.Model):
 
     def send(self,resp):
         msg = self.text.split("\\s")
-        for m in msg: resp.message(m)
+        for m in msg:
+            if "Link:" in m:
+                url = m.replace("Link:","")
+                #resp.message("a").media(url)
+                resp.message("").media(url)
+            else:
+                resp.message(m)
+
+    def getResponse(self,value):
+        filtr = lambda x,y: bool(re.findall("\d\s?-",x)) and y in x
+        msg = [x for x in self.text.split("\n") if filtr(x,value)]
+        if not msg: return value
+        return msg[-1]
 
     def __repr__(self):
         return "<Msg %r>" % self.id
